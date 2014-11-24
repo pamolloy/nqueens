ba0b4724cbf90d57b64814613e777d69c8d27240
Philip Molloy <philip.a.molloy@gmail.com>
2014-11-23 20:34:38 -0500

    Rename perm and perm_list

--- naive_solver.py
+++ naive_solver.py
@@ -5,27 +5,27 @@ import itertools
 import subprocess
 
 def naive_solver(board_size):
-    perm_list = list(itertools.permutations(range(board_size)))
+    permutations = list(itertools.permutations(range(board_size)))
     solutions = []
-    for perm in perm_list:
+    for permutation in permutations:
         try:
             board_size = int(board_size)
 
-            assert len(perm) == board_size
-            assert len(perm) == len(set(perm))
+            assert len(permutation) == board_size
+            assert len(permutation) == len(set(perm))
 
             # Verify the diagonals using brute force
-            for x, y in enumerate(perm):
+            for x, y in enumerate(permutation):
                 for n in range(1, board_size):
                     #print("x=%s, y=%s, n=%s" % (x, y, n))
                     if x + n < board_size and x - n >= 0:
-                        assert y + n != perm[x + n], "%s != %s" % (y + n, perm[x + n])
-                        assert y - n != perm[x + n], "%s != %s" % (y - n, perm[x + n])
+                        assert y + n != permutation[x + n], "%s != %s" % (y + n, permutation[x + n])
+                        assert y - n != permutation[x + n], "%s != %s" % (y - n, permutation[x + n])
                     if x - n < board_size and x - n >= 0:
-                        assert y - n != perm[x - n], "%s != %s" % (y - n, perm[x - n])
-                        assert y + n != perm[x - n], "%s != %s" % (y + n, perm[x - n])
+                        assert y - n != permutation[x - n], "%s != %s" % (y - n, permutation[x - n])
+                        assert y + n != permutation[x - n], "%s != %s" % (y + n, permutation[x - n])
 
-            solutions.append(perm)
+            solutions.append(permutation)
         except: pass
     print len(solutions)
 
